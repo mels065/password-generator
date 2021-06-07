6416c26addfaf7248825a329fee719aa91e113c9
Brandon Mellus <mellusbrandon@gmail.com>
2021-06-06 21:20:39 -0400

    Create prompt for minimum password length

--- script.js
+++ script.js
@@ -10,5 +10,13 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+    // Get minimum password length
+    var minLength = Number.NEGATIVE_INFINITY;
+    while (isNaN(minLength) || minLength < 8) {
+        minLength = Number(prompt("Select minimum password length (cannot be less than 8)", 8));
+    }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
