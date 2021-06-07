f62f670c38fab33456700346346912d5ddce4f74
Brandon Mellus <mellusbrandon@gmail.com>
2021-06-06 21:41:46 -0400

    Create prompt for getting character types in password

--- script.js
+++ script.js
@@ -22,6 +22,30 @@ function generatePassword() {
     while (isNaN(maxLength) || maxLength < minLength || maxLength > 128) {
         maxLength = Number(prompt("Select maximum password length (cannot be more than 128)", 128));
     }
+
+    // Choose options for character types
+    var charTypes = [];
+    var option = null;
+    /*
+        1: lowercase
+        2: uppercase
+        3: numeric
+        4: special
+        5: quit
+    */
+    while (charTypes.length === 0 || option !== 5) {
+        option = Number(prompt("Choose character types (1: lowercase, 2: uppercase, 3: numeric, 4: special, 5: quit)"));
+        switch (option) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                if (!charTypes.includes(option)) charTypes.push(option);
+                break;
+            case 5:
+                if (charTypes.length == 0) charTypes = null;
+        }
+    }
 }
 
 // Add event listener to generate button
